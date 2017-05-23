c716873522f1a05a1086ffd00581f10d4a87f319
jbw0033 <jbw0033@auburn.edu>
2017-05-23 01:08:51 -0700

    Crawler2 for taking files
    This one takes a file of URLs for the input

---
+++ Crawler2/src/TermCounter.java
@@ -0,0 +1,261 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+
+
+/**
+ * Encapsulates a map from search term to frequency (count).
+ * 
+ * @author downey
+ *
+ */
+public class TermCounter {
+	
+	private Map<String, Integer> map;
+	private String label;
+	private static int fileCount;
+	
+	public TermCounter(String label) {
+		this.label = label;
+		this.map = new HashMap<String, Integer>();
+		fileCount = 0;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+	
+	/**
+	 * Returns the total of all counts.
+	 * 
+	 * @return
+	 */
+	public int size() {
+		int total = 0;
+		for (Integer value: map.values()) {
+			total += value;
+		}
+		return total;
+	}
+
+	/**
+	 * Takes a collection of Elements and counts their words.
+	 * 
+	 * @param paragraphs
+	 */
+	public void processElements(Elements paragraphs) {
+		for (Node node: paragraphs) {
+			processTree(node);
+		}
+	}
+	
+	/**
+	 * Finds TextNodes in a DOM tree and counts their words.
+	 * 
+	 * @param root
+	 */
+	public void processTree(Node root) {
+		// NOTE: we could use select to find the TextNodes, but since
+		// we already have a tree iterator, let's use it.
+		for (Node node: new WikiNodeIterable(root)) {
+			if (node instanceof TextNode) {
+//				processText(((TextNode) node).text());
+				writeToFile(((TextNode) node).text());
+			}
+		}
+	}
+	
+	private void writeToFile(String data) {
+		File file;
+		BufferedWriter writer = null;
+		
+		try {
+			file = new File("datafile" + fileCount + ".txt");
+
+			if(!file.exists()) {
+				file.createNewFile();
+			}
+			
+			writer = new BufferedWriter(new FileWriter(file, true));
+			
+			data = data.replaceAll("\\r\\n|\\r|\\n", "$$$$$$");
+			writer.write(data);
+			
+			writer.close();
+		}
+		catch (Exception e) {
+
+			System.out.println("File could not be created.");
+		}
+	}
+
+	/**
+	 * Splits `text` into words and counts them.
+	 * 
+	 * @param text  The text to process.
+	 */
+	public void processText(String text) {
+		// replace punctuation with spaces, convert to lower case, and split on whitespace
+		String[] array = text.replaceAll("\\pP", " ").toLowerCase().split("\\s+");
+		
+		for (int i=0; i<array.length; i++) {
+			String term = array[i];
+			incrementTermCount(term);
+		}
+	}
+
+	/**
+	 * Increments the counter associated with `term`.
+	 * 
+	 * @param term
+	 */
+	public void incrementTermCount(String term) {
+		// System.out.println(term);
+		put(term, get(term) + 1);
+	}
+
+	/**
+	 * Adds a term to the map with a given count.
+	 * 
+	 * @param term
+	 * @param count
+	 */
+	public void put(String term, int count) {
+		map.put(term, count);
+	}
+
+	/**
+	 * Returns the count associated with this term, or 0 if it is unseen.
+	 * 
+	 * @param term
+	 * @return
+	 */
+	public Integer get(String term) {
+		Integer count = map.get(term);
+		return count == null ? 0 : count;
+	}
+
+	/**
+	 * Returns the set of terms that have been counted.
+	 * 
+	 * @return
+	 */
+	public Set<String> keySet() {
+		return map.keySet();
+	}
+	
+	/**
+	 * Print the terms and their counts in arbitrary order.
+	 */
+	public void printCounts() {
+		ValueComparator bvc = new ValueComparator(map);
+		TreeMap<String, Integer> sortedMap = new TreeMap<String, Integer>(bvc);
+		sortedMap.putAll(map);
+		for (String key: sortedMap.keySet()) {
+			Integer count = get(key);
+			System.out.println(key + ", " + count);
+		}
+		System.out.println("Total of all counts = " + size());
+	}
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		System.out.println("Enter the name of a file with URLs: ");
+		String filename = reader.nextLine(); // Scans the next token of the input as an int.
+		
+		reader.close();
+		
+		BufferedReader br = null;
+		FileReader fr = null;
+
+		try {
+
+			fr = new FileReader(filename);
+			br = new BufferedReader(fr);
+
+			String sCurrentLine;
+
+			br = new BufferedReader(new FileReader(filename));
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				String url = sCurrentLine;
+				
+				WikiFetcher wf = new WikiFetcher();
+				Elements paragraphs = wf.fetchWikipedia(url);
+				
+				File file = new File("datafile" + fileCount + ".txt");
+				
+				if(file.exists()) {
+					file.delete();
+				}
+				
+				TermCounter counter = new TermCounter(url);
+				counter.processElements(paragraphs);
+				
+				fileCount++;
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
+		}
+		
+		System.out.println("done");
+//		counter.printCounts();
+	}
+	
+	class ValueComparator implements Comparator<String> {
+	    Map<String, Integer> base;
+
+	    public ValueComparator(Map<String, Integer> base) {
+	        this.base = base;
+	    }
+
+	    // Note: this comparator imposes orderings that are inconsistent with
+	    // equals.
+	    public int compare(String a, String b) {
+	        if (base.get(a) >= base.get(b)) {
+	            return -1;
+	        } else {
+	            return 1;
+	        } // returning 0 would merge keys
+	    }
+	}
+}
